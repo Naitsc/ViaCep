d40e61dc79cc77799e60298ccf61196c590289fb
Diego <diegofu.02@gmail.com>
2022-10-27 18:24:18 -0300

    Initial commit

---
+++ app/src/main/java/com/naitsc/viacep/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.naitsc.viacep
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.gson.Gson
+import com.naitsc.viacep.api.Api
+import com.naitsc.viacep.databinding.ActivityMainBinding
+import com.naitsc.viacep.model.Endereco
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.statusBarColor = Color.parseColor("#FF018786")
+        val actionBar = supportActionBar
+        actionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FF018786")))
+
+
+        //Configurar o Retrofit
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://viacep.com.br/ws/")
+            .build()
+            .create(Api::class.java)
+
+        binding.btBuscarCep.setOnClickListener {
+
+            val cep = binding.editCep.text.toString()
+
+            if (cep.isEmpty()) {
+                Toast.makeText(this, "Preencha o cep!", Toast.LENGTH_SHORT).show()
+
+            } else {
+
+                retrofit.setEndereco(cep).enqueue(object : Callback<Endereco> {
+                    override fun onResponse(call: Call<Endereco>, response: Response<Endereco>) {
+                        if (response.code() == 200) {
+                            val logradouro = response.body()?.logradouro.toString()
+                            val bairro = response.body()?.bairro.toString()
+                            val localidade = response.body()?.localidade.toString()
+                            val uf = response.body()?.uf.toString()
+                            setFormularios(logradouro, bairro, localidade, uf)
+
+                        } else {
+                            Toast.makeText(applicationContext, "Cep inválido!", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<Endereco>, t: Throwable) {
+                        Toast.makeText(applicationContext, "Erro inesperado!", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                })
+            }
+        }
+
+    }
+
+    private fun setFormularios(logradouro: String, bairro: String, localidade: String, uf: String) {
+        binding.editLogradouro.setText(logradouro)
+        binding.editBairro.setText(bairro)
+        binding.editCidade.setText(localidade)
+        binding.editEstado.setText(uf)
+
+    }
+}
---
+++ app/src/main/java/com/naitsc/viacep/model/Endereco.kt
@@ -0,0 +1,9 @@
+package com.naitsc.viacep.model
+
+class Endereco(
+    val logradouro: String? = null,
+    val bairro: String? = null,
+    val localidade: String? = null,
+    val uf: String? = null
+
+)
